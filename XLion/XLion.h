b9a0f618af2b758fbb32c68cf3318abb5c470af0
Anton Paliakov <toxa95401@gmail.com>
2020-06-22 13:07:52 +0300

    Add base project.

---
+++ XLion/XLion.h
@@ -0,0 +1,19 @@
+//
+//  XLion.h
+//  XLion
+//
+//  Created by Anton Polyakov on 6/22/20.
+//  Copyright © 2020 Anton Paliakou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XLion.
+FOUNDATION_EXPORT double XLionVersionNumber;
+
+//! Project version string for XLion.
+FOUNDATION_EXPORT const unsigned char XLionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XLion/PublicHeader.h>
+
+
