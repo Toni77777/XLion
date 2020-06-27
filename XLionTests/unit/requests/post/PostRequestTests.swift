4c8f90ecdd031c3b25268caebcb64839ca0abe9c
Anton Paliakov <toxa95401@gmail.com>
2020-06-27 18:36:10 +0300

    Add base PostRequest tests.

---
+++ XLionTests/unit/requests/post/PostRequestTests.swift
@@ -0,0 +1,20 @@
+//
+//  PostRequestTests.swift
+//  XLionTests
+//
+//  Created by Anton Polyakov on 6/27/20.
+//  Copyright © 2020 Anton Paliakou. All rights reserved.
+//
+
+import XCTest
+
+@testable import XLion
+
+class PostRequestTests: XCTestCase {
+
+    func testWhenInitRequestShouldBeGetMethod() {
+        let postRequest = PostRequest<User>()
+
+        XCTAssertEqual(postRequest.restMethod, HttpMethod.post)
+    }
+}
